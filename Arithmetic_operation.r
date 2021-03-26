f46d48295f5ad6c5572d77a17050f4f7ea31dac1
Ahmad Arsim <79629827+ahmadarsim@users.noreply.github.com>
2021-03-27 02:39:07 +0500

    Arithmetic_operation.r
    here we discuss in deep about the arithmetic operations with multiple examples

---
+++ Arithmetic_operation.r
@@ -0,0 +1,53 @@
+# "+" is used to add the value, vector 
+# "-" is used to subtract the value, subtract the second vector from the first vector
+# "*" is used to multiply the value, multiply the vector
+# "/" is used to divide the value, divide the first vector with the second
+# "%%" give the reminder and quotient of the first vector with the second
+# "^" this will e used as the exponent, or raised to the power
+
+a <- 5
+b<- 6
+c <- a +b
+print(c)
+
+Ali_has_mangos <- 15
+Aslam_has_mangos <- 6
+total_mangos = Ali_has_mangos + Aslam_has_mangos 
+print( 'total mangos=', total_mangos )
+
+x <- c( 12,25,45,65,98 )
+y <- c( 22,65,45,85,25 )
+
+print(x+y)
+
+a <- 625
+b <- 425
+
+c <- a-b
+print(c)
+
+Qasim_have_rs=3625
+Uzair_have_rs =2225
+print("how much have more ruppes uzair than qasim ",Uzair_have_rs - Qasim_have_rs)
+
+
+x <- c(25,45,65,9,57,5,85,96,56,45)
+y <- c(6,52,45,4,55,54,66,54,56,45)
+
+print(x-y)
+
+
+a <- 42
+b<- 62
+print(a*b )
+
+
+length =6
+height = 5
+Area = length * height
+print(Area)
+
+x<-c(23,12,3,21,2,12,1,2,15,4,1 )
+y<-c(2,12,1,2,1,2,1,21,2,13,4 )
+
+print(x*y)
